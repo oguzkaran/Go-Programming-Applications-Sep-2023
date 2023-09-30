06986857abd0be332b24d7cc1034603510afdb57
Oğuz Karan <oguzkaran@csystem.org>
2023-10-01 00:41:04 +0300

    Update

--- src/SampleGoLand/main.go
+++ src/SampleGoLand/main.go
@@ -9,22 +9,6 @@
 */
 package main
 
-import "fmt"
-
-var g_a int32 = 34
-
 func main() {
-	fmt.Println(g_a)
-	foo()
-	fmt.Println(g_a)
-	bar()
-	fmt.Println(g_a)
-}
 
-func foo() {
-	g_a = 10
-}
-
-func bar() {
-	g_a = 20
 }
