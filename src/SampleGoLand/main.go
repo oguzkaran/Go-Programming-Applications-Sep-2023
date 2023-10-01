0880dcd419bc0c7daf3d91087088ecf8053f1315
Oğuz Karan <oguzkaran@csystem.org>
2023-10-01 21:54:57 +0300

    Update

--- src/SampleGoLand/main.go
+++ src/SampleGoLand/main.go
@@ -1,9 +1,7 @@
 /*
 ----------------------------------------------------------------------------------------------------------------------
 
-	Go'da tüm fonksiyonların dışında paket içerisinde bildirilen değişkenlere global değişkenler (global variables)	denir.
-	Global değişkenler aynı dosyadaki tüm fonksiyonlar içerisinde görülebilirdir. Bu duruma ilişkin bazı ayrıntılar
-	ileride ele alınacaktır
+	Sabitler (Constants/Literals):
 
 ----------------------------------------------------------------------------------------------------------------------
 */
