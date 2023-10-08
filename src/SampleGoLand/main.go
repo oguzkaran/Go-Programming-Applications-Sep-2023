b582c0aa265caef8312fb0638e855d876437eba6
Oğuz Karan <oguzkaran@csystem.org>
2023-10-08 18:41:19 +0300

    Update

--- src/SampleGoLand/main.go
+++ src/SampleGoLand/main.go
@@ -1,10 +1,21 @@
 /*
 ------------------------------------------------------------------------------------------------------------------------
 
+	Gerçek sayı türleri için yani f format karakteri ile değerin noktadan sonraki kısmının kaç basamak olarak formatlanacağı
+	belirlenebilir. Geri kalan değerler için bilimsel yuvarlama yapılır. Bunun için f ile % arasında nokta ile birlikte
+	pozitif bir sayı yazılır. Bu sayı kaç basamağın formatlanacağını belirtir
+
 ------------------------------------------------------------------------------------------------------------------------
 */
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	var val float64
+
+	fmt.Print("Input a value:")
+	fmt.Scan(&val)
+
+	fmt.Printf("val = %.10f\n", val)
 }
