bcab40459944b0c7c25c0ced28167082250aceab
Oğuz Karan <oguzkaran@csystem.org>
2023-10-14 23:31:45 +0300

    Update

--- src/SampleGoLand/main.go
+++ src/SampleGoLand/main.go
@@ -1,10 +1,24 @@
 /*
 ------------------------------------------------------------------------------------------------------------------------
 
+	Mantıksal operatörler 3 tanedir: Logical AND (&&), Logical OR(||), Logical NOT(!)
+
 ------------------------------------------------------------------------------------------------------------------------
 */
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	var a, b int
+
+	fmt.Print("Input two numbers:")
+	fmt.Scan(&a, &b)
+
+	fmt.Println(a < b)
+	fmt.Println(a >= b)
+	fmt.Println(a > b)
+	fmt.Println(a <= b)
+	fmt.Println(a == b)
+	fmt.Println(a != b)
 }
