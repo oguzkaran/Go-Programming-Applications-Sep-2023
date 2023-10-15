048a536324f05cfdccd9a7f65d406298a9ff8e90
Oğuz Karan <oguzkaran@csystem.org>
2023-10-15 21:03:45 +0300

    Update

--- src/SampleGoLand/main.go
+++ src/SampleGoLand/main.go
@@ -8,25 +8,6 @@
 
 package main
 
-import "fmt"
-
 func main() {
-	var a int
 
-	fmt.Print("Bir sayı giriniz:")
-	fmt.Scan(&a)
-
-	if isOdd(a) {
-		fmt.Println("Tek sayı!...")
-	} else {
-		fmt.Println("Çift sayı!...")
-	}
-}
-
-func isEven(val int) bool {
-	return val%2 == 0
-}
-
-func isOdd(val int) bool {
-	return !isEven(val)
 }
