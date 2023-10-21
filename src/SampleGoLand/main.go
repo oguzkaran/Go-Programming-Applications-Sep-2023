078c85d8ae540f811cf91599568083f20ea9499d
Oğuz Karan <oguzkaran@csystem.org>
2023-10-21 20:33:40 +0300

    Update

--- src/SampleGoLand/main.go
+++ src/SampleGoLand/main.go
@@ -1,9 +1,8 @@
 /*
 ------------------------------------------------------------------------------------------------------------------------
 
-	Sınıf Çalışması: Klavyeden sıfır girilene kadar alınan int türden sayılardan pozitif olanlarının ve negatif olanlarının
-	ayrı ayrı kaç tane olduklarını ve ayrı ayrı toplamlarını bulan programı yazınız. Hiç pozitif sayı ya da negatif
-	girilmemişse duruma uygun mesajı veriniz
+	Sınıf Çalışması: Parametresi ile aldığı int türden bir değerden büyük ilk Fibonacci sayısını döndüren nextFibonacci
+	fonksiyonunu yazınız ve test ediniz
 
 ------------------------------------------------------------------------------------------------------------------------
 */
@@ -13,55 +12,21 @@ package main
 import "fmt"
 
 func main() {
-	runApp()
+	runFactorialTest()
 }
 
-func runApp() {
-	printResults(findPosNegResults())
-}
-
-func printResults(posSum int, posCount int, negSum int, negCount int) {
-	printPosResult(posSum, posCount)
-	printNegResult(negSum, negCount)
-}
-
-func printPosResult(posSum int, posCount int) {
-	if posCount != 0 {
-		fmt.Printf("You entered %d positive number with total: %d\n", posCount, posSum)
-	} else {
-		fmt.Println("You did not enter a positive number!...")
+func runFactorialTest() {
+	for n := -1; n <= 10; n++ {
+		fmt.Printf("%d! = %d\n", n, factorial(n))
 	}
 }
 
-func printNegResult(negSum int, negCount int) {
-	if negCount != 0 {
-		fmt.Printf("You entered %d negative number with total: %d\n", negCount, negSum)
-	} else {
-		fmt.Println("You did not enter a negative number!...")
-	}
-}
+func factorial(n int) int {
+	result := 1
 
-func findPosNegResults() (int, int, int, int) {
-	posSum, posCount, negSum, negCount := 0, 0, 0, 0
-
-	var val int
-
-	for {
-		fmt.Print("Input a number:")
-		fmt.Scan(&val)
-
-		if val == 0 {
-			break
-		}
-
-		if val > 0 {
-			posSum += val
-			posCount++
-		} else {
-			negSum += val
-			negCount++
-		}
+	for ; n > 1; n-- {
+		result *= n
 	}
 
-	return posSum, posCount, negSum, negCount
+	return result
 }
