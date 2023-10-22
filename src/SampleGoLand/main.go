f7ea00c9a59aa3da463175cb8c7d80c5cccfcfff
Oğuz Karan <oguzkaran@csystem.org>
2023-10-22 18:38:51 +0300

    Update

--- src/SampleGoLand/main.go
+++ src/SampleGoLand/main.go
@@ -12,21 +12,39 @@ package main
 import "fmt"
 
 func main() {
-	runFactorialTest()
+	runFibonacciNumberTest()
 }
 
-func runFactorialTest() {
-	for n := -1; n <= 10; n++ {
-		fmt.Printf("%d! = %d\n", n, factorial(n))
+func runFibonacciNumberTest() {
+	for {
+		var n int
+
+		fmt.Print("n değerini giriniz:")
+		fmt.Scan(&n)
+
+		if n <= 0 {
+			break
+		}
+
+		fmt.Printf("%d. Fibonacci sayısı:%d\n", n, fibonacciNumber(n))
 	}
+
+	fmt.Println("Tekrar yapıyor musunuz?")
 }
 
-func factorial(n int) int {
-	result := 1
-
-	for ; n > 1; n-- {
-		result *= n
+func fibonacciNumber(n int) int {
+	if n <= 2 {
+		return n - 1
 	}
 
-	return result
+	prev1, prev2 := 1, 0
+	value := prev1 + prev2
+
+	for i := 3; i < n; i++ {
+		prev2 = prev1
+		prev1 = value
+		value = prev1 + prev2
+	}
+
+	return value
 }
