3dbd527d5cf669ee578384ff5718b7f5f7f3fbf5
Oğuz Karan <oguzkaran@csystem.org>
2023-10-30 08:25:09 +0300

    Update

--- src/SampleGoLand/main.go
+++ src/SampleGoLand/main.go
@@ -1,10 +1,7 @@
 /*
 ------------------------------------------------------------------------------------------------------------------------
 
-	Bir process için tipik olarak iki tane bellek alanı tahsis edilir: stack, heap. Şüphesiz sisteme yönelik başka
-	bellek alanları da söz konusudur.
 
-	Yerel değişkenler ve fonksiyon parametre değişkenleri stack'de yaratılırlar.
 
 ------------------------------------------------------------------------------------------------------------------------
 */
