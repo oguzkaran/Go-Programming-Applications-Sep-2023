330fb549658e4e6392d480b4706dc87af728c646
Oğuz Karan <oguzkaran@csystem.org>
2023-11-05 20:33:36 +0300

    Update

--- src/SampleGoLand/main.go
+++ src/SampleGoLand/main.go
@@ -18,49 +18,94 @@
 
 		- Tarihin herhangi bir bileşenini değiştiren Day, Month ve Year metotları yazılacaktır
 
+		- Set işlemi yapan metotların geri dönüş değerleri bool türden olacaktır. İşlemin başarılı olup olmadığında göre
+		true ya da false değerine geri döneceklerdir
+
 ------------------------------------------------------------------------------------------------------------------------
 */
 
 package main
+
+import "fmt"
 
 type CSDDate struct {
 	day, month, year int
 }
 
 func New(day, month, year int) *CSDDate {
-	//...
+	if isValidDate(day, month, year) {
+		return &CSDDate{day, month, year}
+	}
+
+	return nil
 }
 
 func (d *CSDDate) IsLeap() bool {
-
+	return isLeapYear(d.year)
 }
 
 func (d *CSDDate) DayOfYear() int {
-
+	return getDayOfYear(d.day, d.month, d.year)
 }
 
-func (d *CSDDate) Set(day, month, year int) {
-
+func (d *CSDDate) DayOfWeek() int {
+	return dayOfWeek(d.day, d.month, d.year)
 }
 
-func (d *CSDDate) Day(day int) {
-
-}
-
-func (d *CSDDate) Month(month int) {
-
-}
-
-func (d *CSDDate) Year(year int) {
-
-}
-
-func dayOfYear(day, month, year int) int {
-	if isValidDate(day, month, year) {
-		return getDayOfYear(day, month, year) + day
+func (d *CSDDate) Set(day, month, year int) bool {
+	if !isValidDate(day, month, year) {
+		return false
 	}
 
-	return -1
+	d.day = day
+	d.month = month
+	d.year = year
+
+	return true
+}
+
+func (d *CSDDate) Day(day int) bool {
+	if isValidDate(day, d.month, d.year) {
+		d.day = day
+		return true
+	}
+
+	return false
+}
+
+func (d *CSDDate) Month(month int) bool {
+	if isValidDate(d.day, month, d.year) {
+		d.month = month
+		return true
+	}
+
+	return false
+}
+
+func (d *CSDDate) Year(year int) bool {
+	if isValidDate(d.day, d.month, year) {
+		d.year = year
+		return true
+	}
+
+	return false
+}
+
+func dayOfWeek(day, month, year int) int {
+	return totalDays(getDayOfYear(day, month, year), year) % 7
+}
+
+func totalDays(dof, year int) int {
+	result := dof
+
+	for y := 1900; y < year; y++ {
+		result += 365
+		if isLeapYear(y) {
+			result++
+		}
+	}
+
+	return result
 }
 
 func getDayOfYear(day, month, year int) int {
@@ -131,5 +176,40 @@ func isLeapYear(year int) bool {
 }
 
 func main() {
+	runApp()
+}
 
+func runApp() {
+	for {
+		var d, m, y int
+		fmt.Print("Tarih bilgisini giriniz:")
+		fmt.Scan(&d, &m, &y)
+		
+		printDateTR(d, m, y)
+
+		if d == 0 && m == 0 && y == 0 {
+			break
+		}
+	}
+}
+
+func printDateTR(day, month, year int) {
+	switch dow := dayOfWeek(day, month, year); dow {
+	case 0:
+		fmt.Printf("%02d.%02d.%04d Pazar\n", day, month, year)
+	case 1:
+		fmt.Printf("%02d.%02d.%04d Pazartesi\n", day, month, year)
+	case 2:
+		fmt.Printf("%02d.%02d.%04d Salı\n", day, month, year)
+	case 3:
+		fmt.Printf("%02d.%02d.%04d Çarşamba\n", day, month, year)
+	case 4:
+		fmt.Printf("%02d.%02d.%04d Perşembe\n", day, month, year)
+	case 5:
+		fmt.Printf("%02d.%02d.%04d Cuma\n", day, month, year)
+	case 6:
+		fmt.Printf("%02d.%02d.%04d Cumartesi\n", day, month, year)
+	default:
+		fmt.Println("Geçersiz tarih!...")
+	}
 }
