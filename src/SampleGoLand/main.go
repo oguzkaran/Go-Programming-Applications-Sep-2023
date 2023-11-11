22b266d39a5dd694d339fbd1a61e427fb03c2c05
Oğuz Karan <oguzkaran@csystem.org>
2023-11-11 20:30:54 +0300

    Update

--- src/SampleGoLand/main.go
+++ src/SampleGoLand/main.go
@@ -1,77 +1,29 @@
 /*
 ------------------------------------------------------------------------------------------------------------------------
 
-	Sınıf Çalışması:
+	Sınıf Çalışması: Parametresi ile aldığı bir yazının tersiniğ döndüren Reverse isimli fonksiyonu yazınız
 
 ------------------------------------------------------------------------------------------------------------------------
 */
 
 package main
 
-type Fraction struct {
-	a, b int
-}
-
-func NewFraction(a, b int) *Fraction {
-
-}
-
-func (f *Fraction) SetNumerator(val int) {
-
-}
-
-func (f *Fraction) SetDenominator(val int) bool {
-
-}
-
-func (f *Fraction) GetRealValue() float64 {
-
-}
-
-func (f *Fraction) Add(other Fraction) Fraction {
-
-}
-
-func (f *Fraction) AddWith(val int) Fraction {
-
-}
-
-func (f *Fraction) Subtract(other Fraction) Fraction {
-
-}
-
-func (f *Fraction) SubtractWith(val int) Fraction {
-
-}
-
-func (f *Fraction) Multiply(other Fraction) Fraction {
-
-}
-
-func (f *Fraction) MultiplyWith(val int) Fraction {
-
-}
-
-func (f *Fraction) Divide(other Fraction) Fraction {
-
-}
-
-func (f *Fraction) DivideWithWith(val int) Fraction {
-
-}
-
-func (f *Fraction) Inc() {
-
-}
-
-func (f *Fraction) Dec() {
-
-}
-
-func (f *Fraction) Print() {
-
-}
-
 func main() {
+
+}
+
+func PadLeadingSpace(s string, newLen int) string {
+
+}
+
+func PadLeading(s string, newLen int, c rune) string {
+
+}
+
+func PadTrailingSpace(s string, newLen int) string {
+
+}
+
+func PadTrailing(s string, newLen int, c rune) string {
 
 }
