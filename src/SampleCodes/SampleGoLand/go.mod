9aa7f46d5c1722dcd6b7fdd15fc054a1b6114cb7
Oğuz Karan <oguzkaran@csystem.org>
2023-11-20 09:07:44 +0300

    Update

--- src/SampleCodes/SampleGoLand/go.mod
+++ src/SampleCodes/SampleGoLand/go.mod
@@ -2,3 +2,4 @@ module SampleGoLand
 
 go 1.21
 
+require github.com/oguzkaran/go-programming-apps-sep-2023-libraries-repository v0.0.0-20231119211959-8a5432a25482 // indirect
--- src/SampleCodes/SampleGoLand/main.go
+++ src/SampleCodes/SampleGoLand/main.go
@@ -1,46 +1,12 @@
 /*
 ------------------------------------------------------------------------------------------------------------------------
 
-	Sınıf Çalışması: SampleGoLand/datetime paketi içerisine tarih ve zamanı temsil eden CSDDateTime isimli yapıyı
-	tasarlayınız ve yazınız
-
-	Açıklamalar: Standart date-time fonksiyonları kullanılmayacaktır
 
 ------------------------------------------------------------------------------------------------------------------------
 */
 
 package main
 
-import (
-	"SampleGoLand/datetime"
-	"fmt"
-)
-
 func main() {
-	runApp()
-}
 
-func runApp() {
-	for {
-		var d, m, y int
-		fmt.Print("Tarih bilgisini giriniz:")
-		fmt.Scan(&d, &m, &y)
-
-		if d == 0 && m == 0 && y == 0 {
-			break
-		}
-
-		printDateTR(d, m, y)
-	}
-}
-
-func printDateTR(day, month, year int) {
-	date := datetime.NewDate(day, month, year)
-
-	if date == nil {
-		fmt.Println("Geçersiz tarih!...")
-		return
-	}
-
-	fmt.Printf("%02d.%02d.%04d %s\n", day, month, year, date.GetDayOfWeekEN())
 }
