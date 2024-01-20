c55a49b91552d5e6a2324bbd4f89eba2f2ababa9
Oğuz Karan <oguzkaran@csystem.org>
2024-01-20 20:30:00 +0300

    Update

--- src/Projects/005-SampleTimeService/app/app.go
+++ src/Projects/005-SampleTimeService/app/app.go
@@ -1,42 +1,115 @@
+/*
+	Sınıf çalışması: Aşağıdaki GET servisleri ekleyiniz
+	- ~/date:O anki tarihi json formatı ile döndüren servis
+	- ~/date/birth?name=Oguz&surname=Karan&day=10&dmonth=9&year=1976: Verilen bilgilere göre aşağıdaki json formatına dönen
+	servis
+		{"fullName": "Oğuz Karan, "birthDate": "10/09/1976", "age":47.39, "today":"20/01/2024"}
+*/
+
 package app
 
 import (
+	"SampleTimeServiceApp/app/jsondata"
+	"encoding/json"
 	"fmt"
 	"net/http"
 	"os"
 	"time"
 )
 
-func timeServerHandler(w http.ResponseWriter, r *http.Request) {
+func checkMethod(w http.ResponseWriter, r *http.Request, message string) bool {
 	if r.Method != http.MethodGet {
-		fmt.Fprintf(w, "%s", "Method must be GET!...")
+		http.Error(w, message, http.StatusForbidden)
+		return false
+	}
+	return true
+}
+
+func checkError(err error, w http.ResponseWriter, message string, statusCode int) bool {
+	if err != nil {
+		http.Error(w, message, statusCode)
+		return false
+	}
+
+	return true
+}
+
+func checkParameter(w http.ResponseWriter, r *http.Request, parameter, message string, statusCode int) (bool, string) {
+	value := r.FormValue(parameter)
+	if value == "" {
+		http.Error(w, message, statusCode)
+		return false, ""
+	}
+
+	return true, value
+}
+
+func timeHandler(w http.ResponseWriter, r *http.Request) {
+	if !checkMethod(w, r, "Method must be GET!...") {
+		return
+	}
+
+	result, name := checkParameter(w, r, "name", `Parameter "name" required!...`, http.StatusBadRequest)
+	if !result {
 		return
 	}
 	now := time.Now()
 	w.WriteHeader(http.StatusOK)
-	ci := ClientInfo{Host: r.Host, Name: "Hello", DateTime: now.Format("02/01/2006 15:04:05")}
-	fmt.Fprintf(w, "%v", ci)
+	ci := jsondata.NewClientInfo(r.RemoteAddr, "Hello "+name, now.Format("02/01/2006 15:04:05"))
+	data, err := json.Marshal(ci)
+	if !checkError(err, w, "Internal server error!...", http.StatusInternalServerError) {
+		return
+	}
+	_, err = fmt.Fprintf(w, "%s", string(data))
+
+	if !checkError(err, w, "Internal server error!...", http.StatusInternalServerError) {
+		return
+	}
 }
 
-func Run() {
+func defaultHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path == "/" {
+		http.Error(w, "No default url", http.StatusNotAcceptable)
+	} else {
+		http.Error(w, "Not found", http.StatusNotFound)
+	}
+}
+
+func checkArguments() {
 	if len(os.Args) != 2 {
 		fmt.Printf("Wrong number of arguments!...")
 		os.Exit(1)
 	}
+}
 
+func createServerInfo(portInfo string) (*http.ServeMux, *http.Server) {
 	handler := http.NewServeMux()
 	server := &http.Server{
-		Addr:    ":" + os.Args[1],
+		Addr:    ":" + portInfo,
 		Handler: handler, IdleTimeout: 10 * time.Second,
 		ReadTimeout:  time.Second,
 		WriteTimeout: time.Second,
 	}
 
-	handler.Handle("/time", http.HandlerFunc(timeServerHandler))
+	return handler, server
+}
 
-	fmt.Println("Service is waiting for a client")
+func addHandlers(handler *http.ServeMux) {
+	handler.Handle("/time", http.HandlerFunc(timeHandler))
+	handler.Handle("/", http.HandlerFunc(defaultHandler))
+}
+
+func startServer(server *http.Server) {
 	err := server.ListenAndServe()
 	if err != nil {
 		fmt.Printf("Message:%s\n", err.Error())
 	}
+}
+
+func Run() {
+	checkArguments()
+	handler, server := createServerInfo(os.Args[1])
+	addHandlers(handler)
+	fmt.Println("Service is waiting for a client")
+	startServer(server)
 }
--- src/Projects/005-SampleTimeService/app/jsondata/clientinfo.go
+++ src/Projects/005-SampleTimeService/app/jsondata/clientinfo.go
@@ -1,7 +1,11 @@
-package app
+package jsondata
 
 type ClientInfo struct {
 	Host     string `json:"host"`
 	Name     string `json:"name"`
 	DateTime string `json:"dateTime"`
+}
+
+func NewClientInfo(host, name, dateTime string) *ClientInfo {
+	return &ClientInfo{host, name, dateTime}
 }
