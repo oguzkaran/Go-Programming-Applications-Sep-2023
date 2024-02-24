db0f4424e9ae6042984b12a9b9105cfb40d9d35f
Oğuz Karan <oguzkaran@csystem.org>
2024-02-24 23:37:58 +0300

    Update

--- src/SampleCodes/SampleGoLand/main.go
+++ src/SampleCodes/SampleGoLand/main.go
@@ -1,33 +1,59 @@
 /*
 ------------------------------------------------------------------------------------------------------------------------
 
-	Aşağıdaki demo örnekte yaratılan thread'leri stack alanları farklı olduğundan i döngü değişkeni de her thread için
-	ayrı olarak yaratılır
+	Aşağıdaki demo örnekteki calculateCounter fonksiyonunda return deyiminden önce UnLock yapılmazsa deadlock oluşur. Go
+	runtime goroutine'ler için deadlock durumlarını yakalar ve panic fonksiyonu çağrılır
 
 ------------------------------------------------------------------------------------------------------------------------
 */
 package main
 
 import (
+	"SampleGoLand/csd/console"
 	"fmt"
-	"time"
+	"math/rand"
+	"sync"
 )
 
-type Sample struct {
-	val int
+type Counter struct {
+	value int32
+	total int64
 }
 
-func goroutineCallback(count int, ps *Sample) {
-	for i := 0; i < count; i++ {
-		ps.val++
+func calculateCounter(counter *Counter, mutex *sync.Mutex) {
+	mutex.Lock()
+	var value = int32(rand.Intn(20) - 10)
+
+	mutex.Lock()
+	counter.value += value
+	counter.total += int64(counter.value)
+	mutex.Unlock()
+	mutex.Unlock()
+}
+
+func goroutineCallback(n int, counter *Counter, wg *sync.WaitGroup, mutex *sync.Mutex) {
+	for i := 0; i < n; i++ {
+		calculateCounter(counter, mutex)
 	}
-	fmt.Println("goroutineCallback ends!...")
+
+	wg.Done()
 }
 
 func main() {
-	s := Sample{}
-	go goroutineCallback(10_000_000, &s)
-	go goroutineCallback(10_000_000, &s)
-	time.Sleep(3 * time.Second)
-	fmt.Printf("value = %d\n", s.val)
+	var wg sync.WaitGroup
+	var mutex sync.Mutex
+
+	count := console.ReadInt("Input count:", "")
+	n := console.ReadInt("Input number of goroutines:", "")
+
+	counter := Counter{}
+	for i := 0; i < n; i++ {
+		wg.Add(1)
+		go goroutineCallback(count, &counter, &wg, &mutex)
+	}
+
+	wg.Wait()
+
+	fmt.Printf("Value:%d\n", counter.value)
+	fmt.Printf("Total:%d\n", counter.total)
 }
