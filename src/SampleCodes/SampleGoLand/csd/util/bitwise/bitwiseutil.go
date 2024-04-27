3e648bf1c41c34974168be9d6d2194fb1801eb29
Oğuz Karan <oguzkaran@csystem.org>
2024-04-27 20:52:13 +0300

    Update

--- src/SampleCodes/SampleGoLand/csd/util/bitwise/bitwiseutil.go
+++ src/SampleCodes/SampleGoLand/csd/util/bitwise/bitwiseutil.go
@@ -4,22 +4,63 @@ import (
 	"fmt"
 )
 
-func BitStr(a int, size int) string {
-	return fmt.Sprintf(fmt.Sprintf("%%0%db", size), a)
+func BitStrUInt8(a uint8) string {
+	return fmt.Sprintf("%08b", a)
 }
 
-func BitStrInt32(a int32) string {
-	return BitStr(int(a), 32)
+func BitStrUInt16(a uint16) string {
+	return fmt.Sprintf("%016b", a)
 }
 
 func BitStrUInt32(a uint32) string {
-	return BitStr(int(a), 32)
-}
-
-func BitStrInt64(a int64) string {
-	return BitStr(int(a), 64)
+	return fmt.Sprintf("%032b", a)
 }
 
 func BitStrUInt64(a uint64) string {
-	return BitStr(int(a), 64)
+	return fmt.Sprintf("%064b", a)
 }
+
+func ClearBitUInt8(a, k uint8) uint8 {
+	return a &^ (uint8(1) << k)
+}
+
+func ClearBitUInt16(a, k uint16) uint16 {
+	return a &^ (uint16(1) << k)
+}
+func ClearBitUInt32(a, k uint32) uint32 {
+	return a &^ (uint32(1) << k)
+}
+
+func ClearBitUInt64(a, k uint64) uint64 {
+	return a &^ (uint64(1) << k)
+}
+
+func IsClearUInt32(a, k uint32) bool {
+	return !IsSetUInt32(a, k)
+}
+
+func IsSetUInt32(a, k uint32) bool {
+	return a&uint32(1)<<k != 0
+}
+
+func SetBitUInt8(a, k uint8) uint8 {
+	return a | uint8(1)<<k
+}
+
+func SetBitUInt16(a, k uint16) uint16 {
+	return a | uint16(1)<<k
+}
+
+func SetBitUInt32(a, k uint32) uint32 {
+	return a | uint32(1)<<k
+}
+
+func SetBitUInt64(a, k uint64) uint64 {
+	return a | uint64(1)<<k
+}
+
+func ToggleBitUInt32(a, k uint32) uint32 {
+	return a ^ uint32(1)<<k
+}
+
+//...
--- src/SampleCodes/SampleGoLand/main.go
+++ src/SampleCodes/SampleGoLand/main.go
@@ -1,8 +1,10 @@
 /*
 ------------------------------------------------------------------------------------------------------------------------
 
-	<< operatörü birinci operandına ilişkin tamsayının bitlerini, ikinci operandına ilişkin sayı kadar pozisyon sola
-	kaydırılmasından oluşan değeri üretir. Sınır dışına çıkan bitler için sağdan sıfıf biti ile besleme yapılır
+	Bir tamsayının belirli bir bitinin değerinin elde edilmesi:
+
+
+	if (a & 1 << k) != 0
 
 ------------------------------------------------------------------------------------------------------------------------
 */
@@ -10,15 +12,20 @@
 package main
 
 import (
+	"SampleGoLand/csd/console"
 	"SampleGoLand/csd/util/bitwise"
 	"fmt"
 )
 
 func main() {
-	var a uint32 = 0x0000001
+	for {
+		a := uint32(console.ReadInt("Input number:", ""))
+		k := uint32(console.ReadInt("Input bir index:", ""))
 
-	for a != 0 {
 		fmt.Println(bitwise.BitStrUInt32(a))
-		a <<= 1
+		fmt.Println(bitwise.BitStrUInt32(bitwise.ToggleBitUInt32(a, k)))
+		if a == 0 {
+			break
+		}
 	}
 }
