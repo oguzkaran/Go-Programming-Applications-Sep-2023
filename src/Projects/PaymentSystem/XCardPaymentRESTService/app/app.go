cd7c06823329bf726dd4b4418fe82d0b57a72bc3
Oğuz Karan <oguzkaran@csystem.org>
2024-06-14 22:07:59 +0300

    Update

--- src/Projects/PaymentSystem/XCardPaymentRESTService/app/app.go
+++ src/Projects/PaymentSystem/XCardPaymentRESTService/app/app.go
@@ -13,12 +13,12 @@ func Run() {
 	/*
 		Payment Manager TCP protocol details:
 		send id as integer value of 4 bytes
-		send string as "R;endpoint". String should send via length means, first send number of character as 4 bytes integer
+		send string as "R;<endpoints>". String should send via length means, first send number of character as 4 bytes integer
 		value, then send characters as byte array
 		receive 1 byte. If its value is 1 (means success) start rest service, otherwise end process
 	*/
 	initApplication()
-	const paymentManagerServerHost = ""
+	const paymentManagerServerHost = "161.97.141.113"
 	const paymentManagerServerPort = 50540
 
 }
